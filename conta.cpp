c2d60d9cd555bde1156448e5a947df1966910e74
Cleison Carvalho <55935573+Konnak@users.noreply.github.com>
2021-05-15 21:12:45 -0300

    Create conta.cpp

---
+++ conta.cpp
@@ -0,0 +1,10 @@
+    #include <iostream>
+    #include "cliente.h"
+    #include "conta.h"
+
+    using namespace std;
+    int main()
+    {
+    Conta conta(1,"Cleison");
+        return 0;
+    }
