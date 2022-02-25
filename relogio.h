ed34f86deb2ccaa501d848fe3392b4e6223c4033
Cleison Carvalho <cleisondecarvalho@hotmail.com>
2022-02-25 12:00:46 -0300

    relogio.h

---
+++ relogio.h
@@ -0,0 +1,18 @@
+#ifndef POINT_H
+#define POINT_H
+
+class relogio {
+  public:
+  
+  relogio(int hora, int minuto);
+  imprime();
+  relogio();
+  ~relogio();
+  
+  private:
+  int _hora;
+  int _minuto;
+  
+};
+
+#endif
